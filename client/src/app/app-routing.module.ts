c123ed266ae84f2d51fa2d114698990ae4b7200d
Keren Wedel <keren.wedel@sap.com>
2023-03-17 09:31:39 +0200

    added route to module

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -9,6 +9,7 @@ import { ProductComponent } from './product/product.component';
 import { ProfileComponent } from './profile/profile.component';
 import { RegisterComponent } from './auth/components/register/register.component';
 import { AuthGuardService } from './guards/auth-guard.service';
+import {PopUpComponent} from './pop-up/pop-up.component';
 
 const routes: Routes = [
   { path: '', component: HomeComponent },
@@ -31,6 +32,11 @@ const routes: Routes = [
     component: OrderHistoryComponent,
     canActivate: [AuthGuardService],
   },
+  {
+    path: 'subscriptions',
+    component: PopUpComponent,
+    canActivate: [AuthGuardService]
+  }
 ];
 
 @NgModule({
